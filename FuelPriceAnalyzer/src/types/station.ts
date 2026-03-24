bc8e173a3d7d343b383385c7b36ace246a46a4b9
Hugo Hernández <105988787+HuguitoH@users.noreply.github.com>
2026-03-24 20:08:46 +0100

    feat: add clean Station interface for domain logic / (raw.ts) Fix folder location

---
+++ FuelPriceAnalyzer/src/types/station.ts
@@ -0,0 +1,17 @@
+export interface Station {
+    id: string;
+    name: string;
+    address: string;
+    locality: string;
+    municipality: string;
+    province: string;
+    postalCode: string;
+    price: number;
+    productId: string;
+    productName: string;
+    lat: number;
+    lon: number;
+    schedule: string;
+    date: Date;
+}
+
